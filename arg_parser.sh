8a05870a15373c165a52ce3012c49d97e16f6e10
Andrey Epifanov <aepifanov@mirantis.com>
2015-06-13 06:10:03 +0400

    Small refactoring
    - Add global var WORKDIR
    - Add quotes for vars
    - Rename test.sh -> test

--- arg_parser.sh
+++ arg_parser.sh
@@ -1,20 +1,19 @@
-#!/bin/bash
 
 function arg_parser {
-    local CONF=${1:?"CONF is mandatory argument"}
-    local ARGS=${2}
-    local DIR=${3:-$PWD}
+    local CONF=${1:?"Please specify CONF"}
+    local ARGS=${2:-""}
+    local WORKDIR=${WORKDIR?"Please specify WORKDIR"}
 
     local FILENAME=$(mktemp)
 
-    pushd ${DIR} &> /dev/null
-    ./arg_parser.py --conf "${CONF}" --filename ${FILENAME} -- ${ARGS}
+    pushd "${WORKDIR}" &> /dev/null
+    ./arg_parser.py --conf "${CONF}" --filename "${FILENAME}" -- ${ARGS}
     popd         &> /dev/null
 
-    if [ -s  ${FILENAME} ]
+    if [[   -s "${FILENAME}" ]]
     then
-        source ${FILENAME}
-        rm ${FILENAME}
+        source "${FILENAME}"
+        rm     "${FILENAME}"
     else
         exit 0
     fi
